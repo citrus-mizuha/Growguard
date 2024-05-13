4a30b6a34e65854d7480109d53521725706c0bc9
citrus-mizuha <citrus.mizuha@outlook.jp>
2024-05-13 21:57:08 +0900

    I changed a lot.

--- Raspberry-pi/setup.py
+++ Raspberry-pi/setup.py
@@ -7,7 +7,7 @@ start = time.perf_counter()
 os.system("pip install -U pip")
 conda_w = os.system("hash conda")
 osx=("macOS","Ubuntu", "CentOS", "Unknown")
-windowss=("Windows")
+windows=("Windows")
 
 def os_type():
     system = platform.system()
@@ -22,7 +22,7 @@ def os_type():
         elif "Raspbian" in distribution:
             return "Raspberry Pi OS"
         else:
-            return "Unknown Linux Distribution"
+            return "Unknown"
     elif system == "Windows":
         return "Windows"
     else:
@@ -31,7 +31,7 @@ def os_type():
 os_type=os_type()
 
 print("\nOS Type:", os_type+"\n")
-if os_type in osx:
+if os_type.lower() in ["macos", "centos", "ubuntu", "unknown"]:
     if conda_w == 0:
         os.system("conda update --all")
         os.system("conda update -n base conda")
@@ -44,7 +44,8 @@ if os_type in osx:
                 break
             else:
                 while True:
-                    if input("It will make new env. name=plant Agree? (y/n)")=="y":
+                    conda_y = input("It will make new env. name=plant Agree? (y/n)")
+                    if conda_y.lower() in ["y", "yes"]:
                         os.system("conda env create -f=conda_env.yml")
                         os.system("conda activate plant")
                         os.system("conda update --all")
@@ -52,12 +53,12 @@ if os_type in osx:
     else:
         while True:
             pyvenv = input("It will make a env class. name=plant Agree? (y/n)")
-            if pyvenv == "y":
+            if pyvenv.lower() in ["y", "yes"]:
                 os.system("source /env/bin/activate")
                 os.system("pip install -r pip_env.txt")
                 print("pip install was end.")
                 break
-if os_type in windowss:
+if os_type.lower() in ["windows"]:
     if os.system("conda")==0:
         os.system("conda update --all")
         os.system("conda update -n base conda")
@@ -70,7 +71,8 @@ if os_type in windowss:
                 break
             else:
                 while True:
-                    if input("It will make new env. name=plant Agree? (y/n)") == "y":
+                    conda = input("It will make new env. name=plant Agree? (y/n)")
+                    if conda.lower() in ["y", "yes"]:
                         os.system("conda env create -f=conda_env.yml")
                         os.system("conda activate plant")
                         os.system("conda update --all")
@@ -78,10 +80,16 @@ if os_type in windowss:
     else:
         while True:
             pyvenv = input("It will make a env class. name=plant Agree? (y/n)")
-            if pyvenv == "y":
+            if pyvenv.lower() in ["y", "yes"]:
                 os.system("\env\bin\activate")
                 os.system("pip install -r pip_env.txt")
                 print("pip install was end.")
                 break
+if os_type.lower() in ["raspbian"]:
+    raspbian = input("It will be build in This Device. Do you want to build? (Both client system and manage system will setup in this Device.) (y/n)")
+    if raspbian.lower() in ["y", "yes"]:
+        print("Starting.... ")
+        time.sleep(10)
+        # また今度書く
 end = time.perf_counter()
 print("Process End. \ntime taken : " + '{:.2f}'.format((end - start) / 60) + " m")
